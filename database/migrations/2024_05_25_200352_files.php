20c7aad40202618326d9924c64bf51ad995bab10
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-25 20:05:09 +0800

    create migration for files

---
+++ database/migrations/2024_05_25_200352_files.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Files extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        //
+        Schema::create(
+            'new_born_record',
+            function (Blueprint $table) {
+                $table->id(); // Auto-incrementing primary key
+                $table->longText('path');
+                $table->string('file');
+                $table->int('case_no');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        //
+    }
+}
