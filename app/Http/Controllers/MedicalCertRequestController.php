7a88396e9cdfffaadf36594ac6f6ac275621b009
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-27 19:44:21 +0800

    created get medcert function fetch

--- app/Http/Controllers/MedicalCertRequestController.php
+++ app/Http/Controllers/MedicalCertRequestController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\MedicalCertRequest;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\DB;
 
 class MedicalCertRequestController extends Controller {
+    public function get_medcert(Request $request) {
+        try {
+            DB::beginTransaction();
+            $medcerts = DB::table('medical_cert_requests')
+                ->select('id', 'full_name', 'doctors_name', 'contact_number', 'case_number', 'description', 'created_at', 'updated_at')
+                ->orderBy('id', 'DESC')
+                ->get();
+            DB::commit();
+
+            if ($medcerts->isEmpty()) {
+                return response()->json(['message' => 'No data found'], 404);
+            }
+
+            return response()->json($medcerts);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => $e->getMessage()], 500);
+        } catch (QueryException $q) {
+            DB::rollBack();
+            return response()->json(['error' => $q->getMessage()], 500);
+        }
+    }
+
+
     public function store(Request $request) {
         $request->validate([
             'full_name' => 'required|string',
