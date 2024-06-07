de41eb5e4aa325b14da8d00746d0b9cc92e29185
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-06-08 01:03:34 +0800

    formatting

--- resources/js/components/pages/masters/RoomView.vue
+++ resources/js/components/pages/masters/RoomView.vue
@@ -1,12 +1,19 @@
 <template>
     <v-container class="container-main pt-0 pl-0" fluid>
         <v-dialog v-model="dialog" persistent max-width="500px">
-            <v-form  id="Insert" ref="Insert" @submit.prevent="toggleSave" enctype="multipart/form-data">
+            <v-form
+                id="Insert"
+                ref="Insert"
+                @submit.prevent="toggleSave"
+                enctype="multipart/form-data"
+            >
                 <v-card>
                     <v-card-title>
                         <span>VIEW ROOM</span>
                         <v-spacer></v-spacer>
-                        <v-icon color="white" @click="closeDialog()">mdi-close</v-icon>
+                        <v-icon color="white" @click="closeDialog()"
+                            >mdi-close</v-icon
+                        >
                     </v-card-title>
                     <v-card-text>
                         <v-container>
@@ -30,8 +37,8 @@
                                 </v-col>
                                 <v-col cols="12">
                                     <v-text-field
-                                    readonly
-                                    disabled
+                                        readonly
+                                        disabled
                                         label="Room Name"
                                         v-model="room.name"
                                         class="required uppercase"
@@ -42,35 +49,37 @@
                                         name="name"
                                     ></v-text-field>
                                 </v-col>
-                                
                                 <v-col cols="12">
-                                    <v-row>
-                                        <template v-for="(bed,index) in room.beds">
-                                            
-                                            <v-col cols="6" :key="index">
-                                                <span>{{bed.vacant?"Occuppied":"Vacant"}}</span>
-                                                <v-text-field
-                                                    readonly
-                                                    disabled
-                                                    :label="'Bed Name ' + (index+1)"
-                                                    v-model="bed.name"
-                                                    class="required uppercase"
-                                                    dense
-                                                    :rules="rules.required"
-                                                    persistent-placeholder
-                                                    outlined
-                                                    name="name"
-                                                ></v-text-field>
-                                            </v-col>
-                                        </template>
-                                    </v-row>
+                                    <template v-for="(bed, index) in room.beds">
+                                        <span>{{
+                                            bed.vacant ? "Occuppied" : "Vacant"
+                                        }}</span>
+                                        <v-text-field
+                                            readonly
+                                            disabled
+                                            :label="'Bed Name ' + (index + 1)"
+                                            v-model="bed.name"
+                                            class="required uppercase"
+                                            dense
+                                            :rules="rules.required"
+                                            persistent-placeholder
+                                            outlined
+                                            name="name"
+                                        ></v-text-field>
+                                    </template>
                                 </v-col>
                             </v-row>
                         </v-container>
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text :disabled="dialogBtn" @click="closeDialog()">Close</v-btn>
+                        <v-btn
+                            color="blue darken-1"
+                            text
+                            :disabled="dialogBtn"
+                            @click="closeDialog()"
+                            >Close</v-btn
+                        >
                     </v-card-actions>
                 </v-card>
             </v-form>
@@ -79,96 +88,93 @@
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
+import { mapActions, mapState } from "vuex";
 export default {
-    props:{
-        room:{
-            type: Object
+    props: {
+        room: {
+            type: Object,
         },
-        dialog:{
-            type:Boolean
+        dialog: {
+            type: Boolean,
         },
     },
     data: () => ({
-        snackbarTimeout:3000,
-        dialogBtn:false,
+        snackbarTimeout: 3000,
+        dialogBtn: false,
     }),
     methods: {
-        refresh(){
-            this.snackbar.show = false
-            location.reload()
+        refresh() {
+            this.snackbar.show = false;
+            location.reload();
         },
-        closeDialog(){
-            this.$emit('closeDialog')
+        closeDialog() {
+            this.$emit("closeDialog");
         },
-        toggleSave(){
-            if(this.$refs.Insert.validate()){
-                this.$emit('toggleSave')
+        toggleSave() {
+            if (this.$refs.Insert.validate()) {
+                this.$emit("toggleSave");
             }
         },
-        addRoom(){
-            this.room.beds.push({})
+        addRoom() {
+            this.room.beds.push({});
         },
-        removeBed(index){
-            this.room.beds.splice(index,1)
-        }
+        removeBed(index) {
+            this.room.beds.splice(index, 1);
+        },
     },
-    computed:{
-        ...mapState([
-            'rules',
-            'selectRoomType',
-        ]),
-        selectMunicipal(){
-            let arr = []
-            this.provinceMaster.filter(data=>{
-                if(data.id === this.patient.province_id){
-                    arr = data.municipality
+    computed: {
+        ...mapState(["rules", "selectRoomType"]),
+        selectMunicipal() {
+            let arr = [];
+            this.provinceMaster.filter((data) => {
+                if (data.id === this.patient.province_id) {
+                    arr = data.municipality;
                 }
-            })
-            return arr
+            });
+            return arr;
         },
-        selectBarangay(){
-            let arr = []
-            this.provinceMaster.filter(data=>{
-                if(data.id === this.patient.province_id){
-                    data.municipality.filter(municipal=>{
-                        if(municipal.id === this.patient.municipality_id){
-                            arr = municipal.barangay
+        selectBarangay() {
+            let arr = [];
+            this.provinceMaster.filter((data) => {
+                if (data.id === this.patient.province_id) {
+                    data.municipality.filter((municipal) => {
+                        if (municipal.id === this.patient.municipality_id) {
+                            arr = municipal.barangay;
                         }
-                    })
+                    });
                 }
-            })
-            return arr
+            });
+            return arr;
         },
-        tempAddressTxt(){
-            let province = ''
-            let municipality = ''
-            let barangay = ''
-            let completeAddress = [this.patient.house_address]
-            this.provinceMaster.filter(data=>{
-                if(data.id === this.patient.province_id){
-                    province = data.name
-                    completeAddress[3] = data.name
-                    data.municipality.filter(municipal=>{
-                        if(municipal.id === this.patient.municipality_id){
-                            municipality = municipal.name
-                            completeAddress[2] = municipal.name
-                            municipal.barangay.filter(bar=>{
-                                if(bar.id === this.patient.barangay_id){
-                                    barangay = bar.name
-                                    completeAddress[1] = bar.name
+        tempAddressTxt() {
+            let province = "";
+            let municipality = "";
+            let barangay = "";
+            let completeAddress = [this.patient.house_address];
+            this.provinceMaster.filter((data) => {
+                if (data.id === this.patient.province_id) {
+                    province = data.name;
+                    completeAddress[3] = data.name;
+                    data.municipality.filter((municipal) => {
+                        if (municipal.id === this.patient.municipality_id) {
+                            municipality = municipal.name;
+                            completeAddress[2] = municipal.name;
+                            municipal.barangay.filter((bar) => {
+                                if (bar.id === this.patient.barangay_id) {
+                                    barangay = bar.name;
+                                    completeAddress[1] = bar.name;
                                 }
-                            })
+                            });
                         }
-                    })
+                    });
                 }
-            })
-            if(!this.patient.house_address){
-                completeAddress.shift()
+            });
+            if (!this.patient.house_address) {
+                completeAddress.shift();
             }
-            console.log('totok',completeAddress)
-            this.patient.address_txt = completeAddress.join(' ')
-            return completeAddress.join(' ')
+            console.log("totok", completeAddress);
+            this.patient.address_txt = completeAddress.join(" ");
+            return completeAddress.join(" ");
             // return this.tempHouse + ' ' + barangay + ' ' + municipality + ' ' + province
         },
     },
@@ -179,10 +185,7 @@ export default {
     //         }
     //     },
     // }
-
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
