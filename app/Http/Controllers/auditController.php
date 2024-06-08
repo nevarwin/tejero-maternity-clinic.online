70cd35452b40c240a0b503476c32d80e82107714
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-06-08 23:35:26 +0800

    formatting

--- app/Http/Controllers/auditController.php
+++ app/Http/Controllers/auditController.php
@@ -1,14 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\auditModel;
 use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
-class auditController extends Controller
-{
-    public function audit_insert(Request $request){
+class auditController extends Controller {
+    public function audit_insert(Request $request) {
         try {
             $audit = new auditModel;
             $audit->name = $request->name;
@@ -17,26 +17,23 @@ class auditController extends Controller
             $audit->drawerLink = $request->drawerLink;
             $audit->position = $request->position;
             $audit->date = $request->date;
-          
+
             $audit->save();
             return 'success';
         } catch (\Exception $e) {
             return $e->getMessage();
-        
-        
+        }
     }
-}
 
-public function audit(Request $request){
-    try {
-        $startDate = $request->start_date;
-        $endDate = $request->end_date;
-        $query = "SELECT * FROM audit_models WHERE created_at BETWEEN '$startDate 00:00:00' AND '$endDate 23:59:59' ORDER BY created_at DESC";
-        $audit = \DB::select($query);
-        return $audit;
-    } catch (\Exception $e) {
-        return $e->getMessage();
+    public function audit(Request $request) {
+        try {
+            $startDate = $request->start_date;
+            $endDate = $request->end_date;
+            $query = "SELECT * FROM audit_models WHERE created_at BETWEEN '$startDate 00:00:00' AND '$endDate 23:59:59' ORDER BY created_at DESC";
+            $audit = DB::select($query);
+            return $audit;
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
     }
-}
-
 }
